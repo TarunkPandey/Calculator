dcd323945a9b3b0e9ec19261f99d7471a463f5d4
Tarun Kumar <tarunpandey2292@gmail.com>
2022-04-20 14:17:31 +0530

    Optimized the buttonClicked(), final modification in JS

--- index.js
+++ index.js
@@ -17,46 +17,96 @@ function isOperator(buttonText) {
   }
 }
 
+// function buttonClicked(buttonText) {
+//   if(displayArea.innerText.length >= 24){
+//     return
+//   }
+//   if (buttonText != "CLR" && buttonText != "DEL" && buttonText != "=") {
+//     if (displayArea.innerText == "0") {
+//       if (buttonText != "0" && buttonText != "00") {
+//         // Don't do anything if button clicked is either '0' or '00'
+//         if (buttonText == "." || isOperator(buttonText)) {
+//           // If button clicked is . or any other operator, then show it as 0 followed by button clicked e.g., 0.5 or 0+5
+//           displayArea.innerText += buttonText;
+//         } else {
+//           displayArea.innerText = buttonText;
+//         }
+//       }
+//     } else {
+//       if (
+//         isOperator(displayArea.innerText[displayArea.innerText.length - 1]) &&
+//         isOperator(buttonText)
+//       ) {
+//         //If last char of displayText is an operator or button clicked is an operator then we replace the already present operator with new one
+//         displayArea.innerText = displayArea.innerText.slice(0, -1) + buttonText;
+//       } else {
+//         displayArea.innerText += buttonText;
+//       }
+//     }
+//   } else {
+//     if (buttonText === "CLR") {
+//       displayArea.innerText = "0";
+//     } else if (buttonText === "DEL") {
+//       displayArea.innerText = displayArea.innerText.slice(0, -1);
+//       if (displayArea.innerText == "") {
+//         displayArea.innerText = "0";
+//       }
+//     } else if (buttonText === "=") {
+//       try {
+//         displayArea.innerText = eval(displayArea.innerText); // eval() calculates whatever arthmetic operation in displayArea
+//       } catch (error) {
+//         displayArea.innerText = "0";
+//       }
+//     }
+//   }
+// }
+
+//Optimized Code buttonClicked()
+
 function buttonClicked(buttonText) {
-  if(displayArea.innerText.length >= 24){
-    return
+  if (displayArea.innerText.length >= 24) {
+    return;
   }
-  if (buttonText != "CLR" && buttonText != "DEL" && buttonText != "=") {
-    if (displayArea.innerText == "0") {
-      if (buttonText != "0" && buttonText != "00") {
-        // Don't do anything if button clicked is either '0' or '00'
-        if (buttonText == "." || isOperator(buttonText)) {
-          // If button clicked is . or any other operator, then show it as 0 followed by button clicked e.g., 0.5 or 0+5
-          displayArea.innerText += buttonText;
-        } else {
-          displayArea.innerText = buttonText;
-        }
-      }
-    } else {
-      if (
-        isOperator(displayArea.innerText[displayArea.innerText.length - 1]) &&
-        isOperator(buttonText)
-      ) {
-        //If last char of displayText is an operator or button clicked is an operator then we replace the already present operator with new one
-        displayArea.innerText = displayArea.innerText.slice(0, -1) + buttonText;
-      } else {
-        displayArea.innerText += buttonText;
-      }
-    }
-  } else {
-    if (buttonText === "CLR") {
+
+  if (buttonText === "CLR") {
+    displayArea.innerText = "0";
+    return;
+  }
+  if (buttonText === "DEL") {
+    displayArea.innerText = displayArea.innerText.slice(0, -1);
+    if (displayArea.innerText == "") {
       displayArea.innerText = "0";
-    } else if (buttonText === "DEL") {
-      displayArea.innerText = displayArea.innerText.slice(0, -1);
-      if (displayArea.innerText == "") {
-        displayArea.innerText = "0";
-      }
-    } else if (buttonText === "=") {
-      try {
-        displayArea.innerText = eval(displayArea.innerText); // eval() calculates whatever arthmetic operation in displayArea
-      } catch (error) {
-        displayArea.innerText = "0";
-      }
     }
+    return;
+  }
+  if (buttonText === "=") {
+    try {
+      displayArea.innerText = eval(displayArea.innerText); // eval() calculates whatever arthmetic operation in displayArea
+    } catch (error) {
+      displayArea.innerText = "0";
+    }
+    return;
+  }
+
+  if (displayArea.innerText === "0") {
+    if (buttonText == "0" && buttonText == "00") return;
+
+    if (buttonText == "." || isOperator(buttonText)) {
+      displayArea.innerText = "0" + buttonText;
+      return;
+    }
+
+    displayArea.innerText = buttonText;
+
+  } 
+  else {
+    if (
+      isOperator(displayArea.innerText[displayArea.innerText.length - 1]) &&
+      isOperator(buttonText)
+    ) {
+      displayArea.innerText = displayArea.innerText.slice(0, -1) + buttonText;
+      return;
+    }
+    displayArea.innerText += buttonText;
   }
 }
